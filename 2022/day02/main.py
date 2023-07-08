5244885f466c57bd7bbced7c67dc6d57ede50ad0
Ícaro Abreu <136408366+oicaroabreu@users.noreply.github.com>
2023-07-07 23:53:39 -0300

    Day Two Part Two

--- 2022/day02/main.py
+++ 2022/day02/main.py
@@ -2,30 +2,54 @@ with open("2022/day02/input.txt", "r",encoding="utf-8") as file:
     content = file.read()
 
 def rps_result(p1: str, p2: str) -> int:
-    if (p1 == 'A' and p2 == 'Z') or \
-    (p1 == 'B' and p2 == 'X') or\
-    (p1 == 'C' and p2 == 'Y'):
+    if (p1 == 'A' and p2 == 'C') or \
+    (p1 == 'B' and p2 == 'A') or\
+    (p1 == 'C' and p2 == 'B'):
         return 0 #lost
     
-    if (p1 == 'A' and p2 == 'Y') or \
-    (p1 == 'B' and p2 == 'Z') or\
-    (p1 == 'C' and p2 == 'X'):
+    if (p1 == 'A' and p2 == 'B') or \
+    (p1 == 'B' and p2 == 'C') or\
+    (p1 == 'C' and p2 == 'A'):
         return 6 #win
     
-    if (p1 == 'A' and p2 == 'X') or \
-    (p1 == 'B' and p2 == 'Y') or\
-    (p1 == 'C' and p2 == 'Z'):
+    if (p1 == 'A' and p2 == 'A') or \
+    (p1 == 'B' and p2 == 'B') or\
+    (p1 == 'C' and p2 == 'C'):
         return 3 #draw
 
+#Part Two
+def hand(elf: str, outcome: str) -> str:
+
+    if outcome == 'Y':
+        return elf
+
+    if outcome == 'Z':
+        if elf == 'A':
+            return 'B' 
+        elif elf == 'B':
+            return 'C'
+        elif elf == 'C':   
+            return 'A'
+    
+    if outcome == 'X':
+        if elf == 'A':
+            return 'C'
+        elif elf == 'B':
+            return 'A'
+        elif elf == 'C':
+            return 'B'
 
 rounds = content.splitlines()
 file.close()
 
 total_score = 0
-scorring = {"X":1,"Y":2,"Z":3}
+scorring = {"A":1,"B":2,"C":3}
 
 for play in rounds:
-    elf, me = play.split()
+    elf, out = play.split()
+
+    #Part Two
+    me = hand(elf, out)
 
     total_score += scorring[me]
     total_score += rps_result(elf, me)
