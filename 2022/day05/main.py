d8eef531a7073f6d7011df3ef7b4ddd443c04501
Ícaro Abreu <136408366+oicaroabreu@users.noreply.github.com>
2023-07-28 22:36:22 -0300

    Day 05 Part One

---
+++ 2022/day05/main.py
@@ -0,0 +1,34 @@
+with open("2022/day05/input.txt", "r",encoding="utf-8") as file:
+    content = file.read()
+
+instructions = content.splitlines()
+file.close()
+
+message = ""
+
+stacks = []
+
+for i in range(9):
+    stack = []
+
+    for line in instructions[:8]:
+        if line[4 * i + 1] != ' ':
+            stack.append(line[4 * i + 1])
+    
+    stacks.append(list(reversed(stack)))  
+
+for i, line in enumerate(instructions[10:]):
+    quantity = int(line.split(' ')[1])
+    from_number = int(line[-6]) - 1
+    to_number = int(line[-1]) - 1
+
+    for move in range(quantity):
+        crane_slot = stacks[from_number].pop()
+        stacks[to_number].append(crane_slot)
+
+for s in stacks:
+    message += s.pop()
+
+print(f"Top crates: {message}")
+
+print("Finished")
